8655b8d3eb93b81ad1a3f40ed13f9825ce367389
Matt Stanley <stanley.t.matthew@gmail.com>
2026-09-25 16:59:11 -0400

    Chore/setup deployment (#8)
    * adds basic prod docker-compose deployment config
    
    * adds docker image build and push action
    
    * fixes copy paste typo in dockerfile name

--- frontend/src/config/local.ts
+++ frontend/src/config/local.ts
@@ -1,7 +1,7 @@
 import AppConfig from "./types";
 
 const config: AppConfig = {
-  apiBaseUrl: new URL("http://localhost:8080"),
+  apiBaseUrl: new URL("http://localhost:4173/api"),
 };
 
 export default config;
